5e2ace555364feb7a1ecd4e2c8175356d2c89523
liam-kimball <56407517+liam-kimball@users.noreply.github.com>
2020-04-07 10:36:30 -0400

    add messaging

--- js/test.js
+++ js/test.js
@@ -32,7 +32,7 @@ let login = new Vue({
                     console.log('Cant find token');
                     //localStorage.removeItem('token');
                 }
-                
+                localStorage.setItem('user_id', this.user_id);
             })
             
         }
@@ -42,11 +42,15 @@ let login = new Vue({
             try {
                 this.token = localStorage.getItem('token');
                 this.user_id = JSON.parse(atob(this.token.split('.')[1])).sub;
+                
             } catch(e) {
                 console.log('Cant find token');
                 //localStorage.removeItem('token');
             }
-            
+            localStorage.setItem('user_id', this.user_id);
+        }
+        else {
+            localStorage.setItem('user_id', null);
         }
     },
 
@@ -64,7 +68,7 @@ let login = new Vue({
     `
 });
 
-let singUp = new Vue({
+let signUp = new Vue({
     el: "#signUp",
     data: {
         first_name: '',
@@ -310,7 +314,7 @@ let threads = new Vue({
             <h4> Threads: </h4>
             <h6> Current thread: {{ current_thread }}</h6>
             <li v-for="thread, i in threads">
-            <input type="radio" id="{{ thread.id }}" :value="thread.id" v-model="current_thread" v-on:change="saveCurrentThread(); messages.updateMessagesList();">
+            <input type="radio" id="{{ thread.id }}" :value="thread.id" v-model="current_thread" v-on:change="saveCurrentThread()">
                 <div class="container p-3 my-3 border">
                     <div v-if="editThread === thread.id">
                         <input v-on:keyup.enter="updateThread(thread)" v-model="thread.name" />
@@ -395,6 +399,98 @@ let users = new Vue({
                     </div>
                 </div>
             </li>
+        </div>
+    `
+});
+
+let message = new Vue({
+    el: "#message",
+    data: {
+        welcomeMessage: 'Chat App',
+        //chats: chatStorage.fetch(),
+        chats: [],
+       
+        addChatMessage: '',
+        conn: new WebSocket('ws://206.189.202.188:8080'),
+        newMessage: ''
+    },
+
+    watch: {
+        //chats: {
+          //  handler: function(chats) {
+            //    chatStorage.save(chats);
+            //}
+        //}
+    },
+
+    methods: {
+        addChat(text){
+            //const text = event.target.value
+           //this.chats.push({text, done: false, id: Date.now()})
+            // event.target.value = ''
+            fetch("https://cors-anywhere.herokuapp.com/" + "http://206.189.202.188:43554/messages/add.json", {
+                body: JSON.stringify({
+                    "body": text,
+                    "thread_id": localStorage.getItem('current_thread')
+                }),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem('token')
+                },
+            })
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                this.token = localStorage.getItem('token');
+                this.user_id = JSON.parse(atob(this.token.split('.')[1])).sub;
+                //this.workspaces.push(data.Work_Space);
+                localStorage.setItem('user_id', data.user_id);
+                this.conn.send(JSON.stringify({"body":text, "user_id":this.user_id}))
+            })  
+            //this.chats = ({message:this.text, id:this.user_id})
+            this.addChatMessage = '';
+            this.newMessage = ''
+        },
+
+        removeChat(id) {
+            //this.chats = this.chats.filter(chat => chat.id !== id)
+        },
+
+
+
+    },
+    mounted() {
+        this.conn.onopen = function(e) {
+            console.log("Connection established!");
+        };
+        this.conn.onmessage = function(e) {
+            console.log(e.data);
+            var data = JSON.parse(e.data);
+            console.log(JSON.stringify(data));
+            if(data.from == "Me") {
+                document.getElementById("chats").innerHTML += '<div class="container bg-info p-3 my-3 border">' + '<h6>User Id: ' + data.user_id + '</h6>' + data.body + '</div>';
+            } else {
+                document.getElementById("chats").innerHTML += '<div class="container bg-secondary p-3 my-3 border">' + '<h6>User Id: ' + data.user_id + '</h6>' + data.body + '</div>';
+            }
+            
+        }
+        this.conn.onclose = function(e) {
+            console.log("Connection Closed!");
+        }
+    },
+    template: `
+        <div class="container bg-dark p-3 my-3 border">
+            <h4> Messages: </h4>
+            <!-- <li v-for="chat, i in chats">
+                <div class="container p-3 my-3 border">
+                        <h4>{{chat.text}}</h4>
+                    </div>
+                </div>
+            </li> -->
+            <input v-model="newMessage" v-on:keyup.enter='addChat(newMessage)'/>
+            <small>adding item...{{newMessage}}</small>
+            <div id="chats"></div>
         </div>
     `
 });
